48503d6303f525655f3be13ee0d94d89c73b5099
Rohit Ganji <grohit.2001@gmail.com>
2021-08-15 22:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,17 @@ import requests
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 cookies = [
-    '',
-    '_ga=GA1.2.1506161081.1587364019; have_account=1; cookieconsent_dismissed=yes; btc_address=1CZ8AgZy1a8duZt4VRyXMqzzu5ZzfvHKQn; password=eda74144c1a1d46c78e2fcbc5075f3bf5085039f8b943b0e21a0c3cd166aa34a; login_auth=a3c0b9b56029ed43ce4d3c19d39d8849186334008655d9ee5da394428fbb174b; __cfduid=d9762eb8e808d2dc89b9b7a35f971a2c01592649691; last_play=1593077988; csrf_token=n8V5wsQnaqy6; _gid=GA1.2.2052395072.1594307843; hide_push_msg=1; _gat=1',
-    '__cfduid=db24ac65e3fcdb63ef3fe080ad8646ab01594773603; csrf_token=wleFaCc9QoGN; _ga=GA1.2.1822743625.1594773608; _gid=GA1.2.214529021.1594773608; hide_push_msg=1; btc_address=12bNszovKQK3dv4jJajesL7q5GRS7G21qG; password=b8115f4301c1d4a8ad59044e89068f6dec739dbc6647b2fae09ce977598ed86b; have_account=1; login_auth=adee9d92de8220e9180887721fc482bcb15c4563b7d8ea6c4e180ef02047acdd; _gat=1'   
-]
+    # Hidden
+    ]
 proxies = [
-    {},
-    {}
+    # Hidden
 ]
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    client = pymongo.MongoClient("mongodb+srv://Rohit:Rohit.2001@cluster0-1g6nx.azure.mongodb.net/test?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://username:password@server")
     db = client.BTC
     doc = []
     for x in db.three.find():
